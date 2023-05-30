b4ae1f190d33ab112d7977843dbe73fbe21ab9a7
some94 <ehdgnl0904@naver.com>
2023-05-30 21:42:23 +0900

    라즈베리에서 윈도우 Workbench로 데이터 전송 테스트

---
+++ db_test.py
@@ -0,0 +1,30 @@
+from random import *
+import time
+import pymysql
+import my_db
+
+count = 0
+sensor_value = 0
+index = 1
+while(1):
+    i = randint(100,999)
+    sensor_value=sensor_value+i
+    time.sleep(1)
+    count=count+1
+    
+    if count==5:
+        conn = pymysql.Connect(host=my_db.host, user='root', password=my_db.password, db='sensor_data', charset='utf8')
+        curr = conn.cursor()
+        res = sensor_value/5
+        
+        sql = "insert into value1 values(' "+str(index)+" ', '"+my_db.host+"',' "+str(res)+" ')"
+        print(sql)
+        print(curr.execute(sql))
+        conn.commit()
+        count=0
+        sensor_value=0
+        index += 1
+        curr.close()
+        conn.close()
+        conn = None
+        curr = None
