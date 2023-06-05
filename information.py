8def0a83649facefe766dde1d16d6f36302064d7
Donghwi Kim <ehdgnl0904@naver.com>
2023-06-05 17:55:45 +0900

    프로젝트 장비 및 상태 정보 관련 모듈

---
+++ information.py
@@ -0,0 +1,15 @@
+# sensor
+dht1 = 'IoT_DHT22_1'
+ir1 = 'IoT_IR_1'
+dht2 = 'AIoT_DHT22_1'
+ir2 = 'AIoT_IR_1'
+
+# equipment
+fan1 = 'IoT_Fan_1'
+bulb1 = 'IoT_Bulb_1'
+fan2 = 'AIoT_Fan_1'
+bulb2 = 'AIoT_Bulb_1'
+
+# status
+detect = '물체가 감지됨'
+non_detect = '감지되지 않음'
