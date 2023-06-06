40d95d2f82d39ae9ad9f370c39cb6d99503e1cc7
Donghwi Kim <ehdgnl0904@naver.com>
2023-06-06 17:08:09 +0900

    DB 쿼리문 추가

--- information.py
+++ information.py
@@ -13,3 +13,13 @@ bulb2 = 'AIoT_Bulb_1'
 # status
 detect = '물체가 감지됨'
 non_detect = '감지되지 않음'
+
+# Database
+iot_dht_sql = """insert into iot_space_dhtsensor values (%s, %s, %s, %s)"""
+iot_ir_sql = """insert into iot_space_irsensor values (%s, %s, %s)"""
+iot_fan_sql = """insert into iot_space_fan values (%s, %s)"""
+iot_bulb_sql = """insert into aiot_space_bulb values (%s, %s)"""
+aiot_dht_sql = """insert into aiot_space_dhtsensor values (%s, %s, %s, %s)"""
+aiot_ir_sql = """insert into aiot_space_irsensor values (%s, %s, %s)"""
+aiot_fan_sql = """insert into aiot_space_fan values (%s, %s)"""
+aiot_bulb_sql = """insert into aiot_space_bulb values (%s, %s)"""
