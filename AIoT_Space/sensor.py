228a8973e05f93ac6017e40c09ac0e64b4ecb4c3
Donghwi Kim <ehdgnl0904@naver.com>
2023-06-09 15:53:32 +0900

    변경된 모듈명 반영 및 TimeCalculator 클래스를 이용한 runtime 코드 수정

--- AIoT_Space/sensor.py
+++ AIoT_Space/sensor.py
@@ -1,78 +1,78 @@
 import RPi.GPIO as GPIO
 import Adafruit_DHT as dht
-import information
 
-from Set import gpioset
-from datetime import datetime
-from Time.now import Time
-from db_sensor import Database
+from Set import set_gpio, set_information
+from Time.time import Time, TimeCalculator
+from Database.db_insert import Database
 
 db = Database()
+tc = TimeCalculator()
+
+def add_number():
+    AIotSpace.num += 1
+    return AIotSpace.num
 
 class AIotSpace:
     num = 1
 
-    def addNumber(self):
-        AIotSpace.num += 1
-        return AIotSpace.num
-
     def run(self):
         fan_status = 0
-        fan_start_time = ""
         fan_current_status = fan_status
 
         bulb_status = 0
-        bulb_start_time = ""
         bulb_current_status = bulb_status
 
         timestamp = Time.nowtime(self)
-        humidity, temperature = dht.read_retry(dht.DHT22, gpioset.aiot_dht)
-        aiot_ir_state = GPIO.input(gpioset.aiot_ir)
+        humidity, temperature = dht.read_retry(dht.DHT22, set_gpio.aiot_dht)
+        aiot_ir_state = GPIO.input(set_gpio.aiot_ir)
 
         if aiot_ir_state == 0:
-            AIotSpace.addNumber(self)
+            add_number()
             bulb_status = 1
             bulb_current_status = bulb_status
-            bulb_start_time = timestamp
+            tc.aiot_bulb_start_time()
 
-            db.aiotIrInsert(AIotSpace.num, information.ir2, timestamp, information.detect)
-            print(Time.nowtime(self), "-- Signal detected!")
-            GPIO.output(gpioset.aiot_ir_fan, GPIO.LOW)
+            db.aiotIrInsert(AIotSpace.num, set_information.ir2, timestamp, set_information.detect)
+            print(timestamp, "-- Signal detected!")
+            GPIO.output(set_gpio.aiot_ir_fan, GPIO.LOW)
+
         elif aiot_ir_state == 1:
-            AIotSpace.addNumber(self)
+            add_number()
             bulb_status = 0
             if bulb_status != bulb_current_status:
-                AIotSpace.addNumber(self)
-                bulb_stop_time = datetime.now()
-                bulb_runtime = bulb_stop_time - bulb_start_time
-                db.aiotBulbInsert(AIotSpace.num, information.bulb2, bulb_runtime)
+                add_number()
+                tc.aiot_bulb_stop_time()
+                bulb_runtime = tc.aiot_bulb_runtime()
+                print(bulb_runtime)
+                db.aiotBulbInsert(AIotSpace.num, set_information.bulb2, bulb_runtime)
                 bulb_current_status = 0
 
-            db.aiotIrInsert(AIotSpace.num, information.ir2, timestamp, information.detect)
-            print(Time.nowtime(self), "-- Signal not detected!")
-            GPIO.output(gpioset.aiot_ir_fan, GPIO.HIGH)
+            db.aiotIrInsert(AIotSpace.num, set_information.ir2, timestamp, set_information.detect)
+            print(timestamp, "-- Signal not detected!")
+            GPIO.output(set_gpio.aiot_ir_fan, GPIO.HIGH)
 
         print("Temp2={0:0.1f}*C Humidity2={1:0.1f}%".format(temperature, humidity))
 
         if temperature > 30.0:
-            AIotSpace.addNumber(self)
+            add_number()
             fan_status = 1
             fan_current_status = fan_status
-            fan_start_time = timestamp
+            tc.aiot_fan_start_time()
 
-            db.aiotDhtInsert(AIotSpace.num, information.dht2, timestamp, temperature, humidity)
+            db.aiotDhtInsert(AIotSpace.num, set_information.dht2, timestamp, temperature, humidity)
             print("온도가 높으므로 쿨링팬을 작동합니다.")
-            GPIO.output(gpioset.aiot_dht_fan, GPIO.LOW)
+            GPIO.output(set_gpio.aiot_dht_fan, GPIO.LOW)
+
         elif temperature <= 30.0:
-            AIotSpace.addNumber(self)
+            add_number()
             fan_status = 0
             if fan_status != fan_current_status:
-                AIotSpace.addNumber(self)
-                fan_stop_time = datetime.now()
-                fan_runtime = fan_stop_time - fan_start_time
-                db.aiotFanInsert(AIotSpace.num, information.fan2, fan_runtime)
+                add_number()
+                tc.aiot_fan_stop_time()
+                fan_runtime = tc.aiot_fan_runtime()
+                db.aiotFanInsert(AIotSpace.num, set_information.fan2, fan_runtime)
                 fan_current_status = 0
 
-            db.aiotDhtInsert(AIotSpace.num, information.dht2, timestamp, temperature, humidity)
+            db.aiotDhtInsert(AIotSpace.num, set_information.dht2, timestamp, temperature, humidity)
             print("온도가 낮으므로 쿨링팬이 작동하지 않습니다.")
-            GPIO.output(gpioset.aiot_dht_fan, GPIO.HIGH)
+            GPIO.output(set_gpio.aiot_dht_fan, GPIO.HIGH)
--- IoT_Space/sensor.py
+++ IoT_Space/sensor.py
@@ -1,77 +1,78 @@
 import RPi.GPIO as GPIO
 import Adafruit_DHT as dht
-import information
 
-from Set import gpioset
-from datetime import datetime
-from Time.now import Time
-from db_sensor import Database
+from Set import set_gpio, set_information
+from Time.time import Time, TimeCalculator
+from Database.db_insert import Database
 
 db = Database()
+tc = TimeCalculator()
+
+def add_number():
+    IoTSpace.num += 1
+    return IoTSpace.num
 
 class IoTSpace:
     num = 1
-    def addNumber(self):
-        IoTSpace.num += 1
-        return IoTSpace.num
 
     def run(self):
         fan_status = 0
-        fan_start_time = ""
         fan_current_status = fan_status
 
         bulb_status = 0
-        bulb_start_time = ""
         bulb_current_status = bulb_status
 
         timestamp = Time.nowtime(self)
-        humidity, temperature = dht.read_retry(dht.DHT22, gpioset.iot_dht)
-        iot_ir_state = GPIO.input(gpioset.iot_ir)
+        humidity, temperature = dht.read_retry(dht.DHT22, set_gpio.iot_dht)
+        iot_ir_state = GPIO.input(set_gpio.iot_ir)
 
         if iot_ir_state == 0:
-            IoTSpace.addNumber(self)
+            add_number()
             bulb_status = 1
             bulb_current_status = bulb_status
-            bulb_start_time = timestamp
+            tc.iot_bulb_start_time()
 
-            db.iotIrInsert(IoTSpace.num, information.ir1, timestamp, information.detect)
+            db.iotIrInsert(IoTSpace.num, set_information.ir1, timestamp, set_information.detect)
             print(timestamp, "-- Signal detected!")
-            GPIO.output(gpioset.iot_ir_fan, GPIO.LOW)
+            GPIO.output(set_gpio.iot_ir_fan, GPIO.LOW)
+
         elif iot_ir_state == 1:
-            IoTSpace.addNumber(self)
+            add_number()
             bulb_status = 0
             if bulb_status != bulb_current_status:
-                IoTSpace.addNumber(self)
-                bulb_stop_time = datetime.now()
-                bulb_runtime = bulb_stop_time - bulb_start_time
-                db.iotBulbInsert(IoTSpace.num, information.bulb1, bulb_runtime)
+                add_number()
+                tc.iot_bulb_stop_time()
+                bulb_runtime = tc.iot_bulb_runtime()
+                print(bulb_runtime)
+                db.iotBulbInsert(IoTSpace.num, set_information.bulb1, bulb_runtime)
                 bulb_current_status = 0
 
-            db.iotIrInsert(IoTSpace.num, information.ir1, timestamp, information.non_detect)
+            db.iotIrInsert(IoTSpace.num, set_information.ir1, timestamp, set_information.non_detect)
             print(timestamp, "-- Signal not detected!")
-            GPIO.output(gpioset.iot_ir_fan, GPIO.HIGH)
+            GPIO.output(set_gpio.iot_ir_fan, GPIO.HIGH)
 
         print("Temp1={0:0.1f}*C Humidity1={1:0.1f}%".format(temperature, humidity))
 
         if temperature > 30.0:
-            IoTSpace.addNumber(self)
+            add_number()
             fan_status = 1
             fan_current_status = fan_status
-            fan_start_time = timestamp
+            tc.iot_fan_start_time()
 
-            db.iotDhtInsert(IoTSpace.num, information.dht1, timestamp, temperature, humidity)
+            db.iotDhtInsert(IoTSpace.num, set_information.dht1, timestamp, temperature, humidity)
             print("온도가 높으므로 쿨링팬을 작동합니다.")
-            GPIO.output(gpioset.iot_dht_fan, GPIO.LOW)
+            GPIO.output(set_gpio.iot_dht_fan, GPIO.LOW)
+
         elif temperature <= 30.0:
-            IoTSpace.addNumber(self)
+            add_number()
             fan_status = 0
             if fan_status != fan_current_status:
-                IoTSpace.addNumber(self)
-                fan_stop_time = datetime.now()
-                fan_runtime = fan_stop_time - fan_start_time
-                db.iotFanInsert(IoTSpace.num, information.fan1, fan_runtime)
+                add_number()
+                tc.iot_fan_stop_time()
+                fan_runtime = tc.iot_fan_runtime()
+                db.iotFanInsert(IoTSpace.num, set_information.fan1, fan_runtime)
                 fan_current_status = 0
 
-            db.iotDhtInsert(IoTSpace.num, information.dht1, timestamp, temperature, humidity)
+            db.iotDhtInsert(IoTSpace.num, set_information.dht1, timestamp, temperature, humidity)
             print("온도가 낮으므로 쿨링팬이 작동하지 않습니다.")
-            GPIO.output(gpioset.iot_dht_fan, GPIO.HIGH)
+            GPIO.output(set_gpio.iot_dht_fan, GPIO.HIGH)
