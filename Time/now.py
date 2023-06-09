c64a65d6c0367b89ef6978860c8f28636725a7c8
Donghwi Kim <ehdgnl0904@naver.com>
2023-06-09 15:50:28 +0900

    runtime 로직에 대한 클래스 추가

--- Time/now.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-class Time:
-
-    def nowtime(self):
-        now = datetime.now().strftime('%Y:%m:%d %H:%M:%S')
-        return now
---
+++ Time/time.py
@@ -0,0 +1,64 @@
+from datetime import datetime
+
+class Time:
+    def nowtime(self):
+        now = datetime.now().strftime('%Y:%m:%d %H:%M:%S')
+        return now
+
+class TimeCalculator:
+    def __init__(self):
+        self.iot_bulb_start_time = None
+        self.iot_bulb_stop_time = None
+
+        self.iot_fan_start_time = None
+        self.iot_fan_stop_time = None
+
+        self.aiot_bulb_start_time = None
+        self.aiot_bulb_stop_time = None
+
+        self.aiot_fan_start_time = None
+        self.aiot_fan_stop_time = None
+
+    def iot_bulb_start_time(self):
+        self.iot_bulb_start_time = datetime.now()
+
+    def iot_bulb_stop_time(self):
+        self.iot_bulb_stop_time = datetime.now()
+
+    def iot_fan_start_time(self):
+        self.iot_fan_start_time = datetime.now()
+
+    def iot_fan_stop_time(self):
+        self.iot_fan_stop_time = datetime.now()
+
+    def aiot_bulb_start_time(self):
+        self.aiot_bulb_start_time = datetime.now()
+
+    def aiot_bulb_stop_time(self):
+        self.aiot_bulb_stop_time = datetime.now()
+
+    def aiot_fan_start_time(self):
+        self.aiot_fan_start_time = datetime.now()
+
+    def aiot_fan_stop_time(self):
+        self.aiot_fan_stop_time = datetime.now()
+
+    def iot_bulb_runtime(self):
+        time_difference = self.iot_bulb_stop_time - self.iot_bulb_start_time
+        runtime = time_difference.total_seconds()
+        return runtime
+
+    def iot_fan_runtime(self):
+        time_difference = self.iot_fan_stop_time - self.iot_fan_start_time
+        runtime = time_difference.total_seconds()
+        return runtime
+
+    def aiot_bulb_runtime(self):
+        time_difference = self.aiot_bulb_stop_time - self.aiot_bulb_start_time
+        runtime = time_difference.total_seconds()
+        return runtime
+
+    def aiot_fan_runtime(self):
+        time_difference = self.aiot_fan_stop_time - self.aiot_fan_start_time
+        runtime = time_difference.total_seconds()
+        return runtime
