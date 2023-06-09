dcb23a7cd467d047f5a5095b4f9a5f5653c565c7
Donghwi Kim <ehdgnl0904@naver.com>
2023-06-09 17:22:37 +0900

    변수명 및 코드 배열 수정

--- Set/set_gpio.py
+++ Set/set_gpio.py
@@ -2,11 +2,11 @@ import RPi.GPIO as GPIO
 
 iot_ir = 18
 iot_dht = 23
-iot_ir_fan = 2
+iot_ir_bulb = 2
 iot_dht_fan = 3
 aiot_ir = 24
 aiot_dht = 25
-aiot_ir_fan = 4
+aiot_ir_bulb = 4
 aiot_dht_fan = 17
 
 class MySet:
@@ -16,7 +16,7 @@ class MySet:
     def setup(self):
         GPIO.setup(iot_ir, GPIO.IN)
         GPIO.setup(aiot_ir, GPIO.IN)
-        GPIO.setup(iot_ir_fan, GPIO.OUT)
+        GPIO.setup(iot_ir_bulb, GPIO.OUT)
         GPIO.setup(iot_dht_fan, GPIO.OUT)
-        GPIO.setup(aiot_ir_fan, GPIO.OUT)
+        GPIO.setup(aiot_ir_bulb, GPIO.OUT)
         GPIO.setup(aiot_dht_fan, GPIO.OUT)
--- Time/time.py
+++ Time/time.py
@@ -25,11 +25,21 @@ class TimeCalculator:
     def iot_bulb_stop_time(self):
         self.iot_bulb_stop_time = datetime.now()
 
+    def iot_bulb_runtime(self):
+        time_difference = self.iot_bulb_stop_time - self.iot_bulb_start_time
+        runtime = time_difference.total_seconds()
+        return runtime
+
     def iot_fan_start_time(self):
         self.iot_fan_start_time = datetime.now()
 
     def iot_fan_stop_time(self):
         self.iot_fan_stop_time = datetime.now()
+
+    def iot_fan_runtime(self):
+        time_difference = self.iot_fan_stop_time - self.iot_fan_start_time
+        runtime = time_difference.total_seconds()
+        return runtime
 
     def aiot_bulb_start_time(self):
         self.aiot_bulb_start_time = datetime.now()
@@ -37,26 +47,16 @@ class TimeCalculator:
     def aiot_bulb_stop_time(self):
         self.aiot_bulb_stop_time = datetime.now()
 
+    def aiot_bulb_runtime(self):
+        time_difference = self.aiot_bulb_stop_time - self.aiot_bulb_start_time
+        runtime = time_difference.total_seconds()
+        return runtime
+
     def aiot_fan_start_time(self):
         self.aiot_fan_start_time = datetime.now()
 
     def aiot_fan_stop_time(self):
         self.aiot_fan_stop_time = datetime.now()
-
-    def iot_bulb_runtime(self):
-        time_difference = self.iot_bulb_stop_time - self.iot_bulb_start_time
-        runtime = time_difference.total_seconds()
-        return runtime
-
-    def iot_fan_runtime(self):
-        time_difference = self.iot_fan_stop_time - self.iot_fan_start_time
-        runtime = time_difference.total_seconds()
-        return runtime
-
-    def aiot_bulb_runtime(self):
-        time_difference = self.aiot_bulb_stop_time - self.aiot_bulb_start_time
-        runtime = time_difference.total_seconds()
-        return runtime
 
     def aiot_fan_runtime(self):
         time_difference = self.aiot_fan_stop_time - self.aiot_fan_start_time
