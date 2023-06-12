6e8cdbd5e44c126e059df55e2b59e513332ff8d4
Donghwi Kim <ehdgnl0904@naver.com>
2023-06-12 18:11:09 +0900

    IoT_Space/sensor.py line 41, TypeError 오류 수정

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import RPi.GPIO as GPIO
 from Set.set_gpio import MySet
 from IoT_Space.sensor import IoTSpace
 from AIoT_Space.sensor import AIoTSpace
+from Time.time import TimeCalculator
 
 
 class Controller:
@@ -12,6 +13,7 @@ class Controller:
         MySet.setup(self)
         self.iot_space = IoTSpace()
         self.aiot_space = AIoTSpace()
+        self.tc = TimeCalculator()
 
     def run(self):
         while True:
