cae1a037fe11af2ffd22901bdcc99312498f88b8
Donghwi Kim <ehdgnl0904@naver.com>
2023-06-13 16:03:07 +0900

    id 컬럼 로직 제거 후 전력 컬럼 테이블 이동(전력 -> 전구, 팬)

--- Database/db_insert.py
+++ Database/db_insert.py
@@ -13,60 +13,70 @@ class Database:
                                   charset='utf8')
         self.cursor = self.db.cursor()
 
-    # iot_space 온습도 센서의 상태 Insert
-    def iotDhtInsert(self, id, name, timestamp, temperature, humidity):
+    # iot_space 온습도 센서
+    def iotDhtInsert(self, name, timestamp, temperature, humidity):
         sql = set_information.iot_dht_sql
-        self.cursor.execute(sql, (id, name, timestamp, temperature, humidity))
+        self.cursor.execute(sql, (name, timestamp, temperature, humidity))
         self.db.commit()
 
-    # iot_space 적외선 센서의 상태 Insert
-    def iotIrInsert(self, id, name, timestamp, status):
+    # iot_space 적외선 센서
+    def iotIrInsert(self, name, timestamp, status):
         sql = set_information.iot_ir_sql
-        self.cursor.execute(sql, (id, name, timestamp, status))
+        self.cursor.execute(sql, (name, timestamp, status))
         self.db.commit()
 
-    # iot_space 쿨링팬의 상태 Insert
-    def iotFanInsert(self, id, name, runtime):
+    # iot_space 쿨링팬
+    def iotFanInsert(self, name, runtime, fan_consumption, daily, month):
         sql = set_information.iot_fan_sql
-        self.cursor.execute(sql, (id, name, runtime))
+        self.cursor.execute(sql, (name, runtime, fan_consumption, daily, month))
         self.db.commit()
 
-    # iot_space 전구의 상태 Insert
-    def iotBulbInsert(self, id, name, runtime):
+    # iot_space 전구
+    def iotBulbInsert(self, name, runtime, bulb_consumption, daily, month):
         sql = set_information.iot_bulb_sql
-        self.cursor.execute(sql, (id, name, runtime))
+        self.cursor.execute(sql, (name, runtime, bulb_consumption, daily, month))
         self.db.commit()
 
-    def iotConInsert(self, id, consumption, daily, month):
-        sql = set_information.iot_con_sql
-        self.cursor.execute(sql, (id, consumption, daily, month))
+    # iot_space 일월별 전력소비량
+    def iotConInsert(self):
+        self.cursor.execute("select daily, sum(fan_consumption) from iot_space_fan group by  daily")
+        result = self.cursor.fetchall()
+        for row in result:
+            fan_consumption = row[1]
+            self.cursor.execute("update iot_space_consumption set daily_consumption = ?", (fan_consumption, ))
+        self.cursor.execute("select daily, sum(bulb_consumption) from iot_space_bulb group by  daily")
+        result = self.cursor.fetchall()
+        for row in result:
+            bulb_consumption = row[1]
+            self.cursor.execute("update iot_space_consumption set daily_consumption = ?", (bulb_consumption, ))
         self.db.commit()
 
-    # aiot_space 온습도 센서의 상태 Insert
-    def aiotDhtInsert(self, id, name, timestamp, temperature, humidity):
+    # aiot_space 온습도 센서
+    def aiotDhtInsert(self, name, timestamp, temperature, humidity):
         sql = set_information.aiot_dht_sql
-        self.cursor.execute(sql, (id, name, timestamp, temperature, humidity))
+        self.cursor.execute(sql, (name, timestamp, temperature, humidity))
         self.db.commit()
 
-    # aiot_space 적외선 센서의 상태 Insert
-    def aiotIrInsert(self, id, name, timestamp, status):
+    # aiot_space 적외선 센서
+    def aiotIrInsert(self, name, timestamp, status):
         sql = set_information.aiot_ir_sql
-        self.cursor.execute(sql, (id, name, timestamp, status))
+        self.cursor.execute(sql, (name, timestamp, status))
         self.db.commit()
 
-    # aiot_space 쿨링팬의 상태 Insert
-    def aiotFanInsert(self, id, name, runtime):
+    # aiot_space 쿨링팬
+    def aiotFanInsert(self, name, runtime, fan_consumption, daily, month):
         sql = set_information.aiot_fan_sql
-        self.cursor.execute(sql, (id, name, runtime))
+        self.cursor.execute(sql, (name, runtime, fan_consumption, daily, month))
         self.db.commit()
 
-    # aiot_space 전구의 상태 Insert
-    def aiotBulbInsert(self, id, name, runtime):
+    # aiot_space 전구
+    def aiotBulbInsert(self, name, runtime, bulb_consumption, daily, month):
         sql = set_information.aiot_bulb_sql
-        self.cursor.execute(sql, (id, name, runtime))
+        self.cursor.execute(sql, (name, runtime, bulb_consumption, daily, month))
         self.db.commit()
 
-    def aiotConInsert(self, id, consumption, daily, month):
+    # aiot_space 일월별 전력소비량
+    def aiotConInsert(self, daily_consumption, monthly_consumption):
         sql = set_information.aiot_con_sql
-        self.cursor.execute(sql, (id, consumption, daily, month))
+        self.cursor.execute(sql, (daily_consumption, monthly_consumption))
         self.db.commit()
--- IoT_Space/sensor.py
+++ IoT_Space/sensor.py
@@ -8,10 +8,6 @@ from Database.db_insert import Database
 db = Database()
 tc = TimeCalculator()
 
-def add_number():
-    IoTSpace.num += 1
-    return IoTSpace.num
-
 # 팬과 전구의 상태
 class DeviceStatus:
     def __init__(self, initial_status):
@@ -25,7 +21,6 @@ class DeviceStatus:
         return self.current_status
 
 class IoTSpace:
-    num = 1
 
     def __init__(self):
         self.fan_status = DeviceStatus(0)
@@ -40,65 +35,54 @@ class IoTSpace:
             if self.bulb_status.get_current_status() != 1:      # 전구가 꺼져있는 상태일 때(전구 상태가 0이었을 때)
                 tc.set_iot_bulb_start_time()        # 전구가 켜진 시간을 받아온다
 
-            add_number()
             self.bulb_status.update_status(1)       # 전구의 현재 상태를 1(켜짐)로 바꾼다
             print(timestamp, "-- Signal detected!")
 
-            db.iotIrInsert(IoTSpace.num, set_information.ir1, timestamp, set_information.detect)
+            db.iotIrInsert(set_information.ir1, timestamp, set_information.detect)
             GPIO.output(set_gpio.iot_ir_bulb, GPIO.LOW)      # 전구를 작동시킨다
 
         elif iot_ir_state == 1:     # IR 센서에 물체가 감지되지 않았을 때
             if self.bulb_status.get_current_status() != 0:      # 전구가 켜져있다가 꺼졌을 때(전구 상태가 1이었을 때)
-                add_number()
+
                 tc.set_iot_bulb_stop_time()     # 전구가 꺼진 시간을 받아온다
                 bulb_runtime = tc.set_iot_bulb_runtime()        # 전구가 작동한 시간을 받아온다
-                print(bulb_runtime)
-                db.iotBulbInsert(IoTSpace.num, set_information.bulb1, bulb_runtime)
+                bulb_consumption = bulb_runtime * 0.00694  # 전구의 초당 전력소비량을 추출한다
+                today = Time.today_day(self)    # 오늘 날짜의  일을 받아온다
+                month = Time.today_month(self)  # 오늘 날짜의 월을 받아온다
+                db.iotBulbInsert(set_information.bulb1, bulb_runtime, bulb_consumption, today, month)
                 self.bulb_status.update_status(0)       # 전구의 상태를 다시 0(꺼짐)으로 바꾼다
 
-                add_number()
-                bulb_consumption = bulb_runtime * 0.00694
-                today = Time.today_day(self)
-                month = Time.today_month(self)
-                db.iotConInsert(IoTSpace.num, bulb_consumption, today, month)
-
-            add_number()
             self.bulb_status.update_status(0)  # 전구의 현재 상태를 0(꺼짐)으로 설정한다
             print(timestamp, "-- Signal not detected!")
 
-            db.iotIrInsert(IoTSpace.num, set_information.ir1, timestamp, set_information.non_detect)
+            db.iotIrInsert(set_information.ir1, timestamp, set_information.non_detect)
             GPIO.output(set_gpio.iot_ir_bulb, GPIO.HIGH)
 
-        print("Temp1={0:0.1f}*C Humidity1={1:0.1f}%".format(temperature, humidity))
+        print("IoT_Temperature={0:0.1f}*C IoT_Humidity={1:0.1f}%".format(temperature, humidity))
 
-        if temperature > 30.0:
+        if temperature > 21.0:
             if self.fan_status.get_current_status() != 1:
                 tc.set_iot_fan_start_time()
 
-            add_number()
             self.fan_status.update_status(1)
             print("온도가 높으므로 쿨링팬을 작동합니다.")
 
-            db.iotDhtInsert(IoTSpace.num, set_information.dht1, timestamp, temperature, humidity)
+            db.iotDhtInsert(set_information.dht1, timestamp, temperature, humidity)
             GPIO.output(set_gpio.iot_dht_fan, GPIO.LOW)
 
-        elif temperature <= 30.0:
+        elif temperature <= 21.0:
             if self.fan_status.get_current_status() != 0:
-                add_number()
+
                 tc.set_iot_fan_stop_time()
                 fan_runtime = tc.set_iot_fan_runtime()
-                db.iotFanInsert(IoTSpace.num, set_information.fan1, fan_runtime)
-                self.fan_status.update_status(0)
-
-                add_number()
                 fan_consumption = fan_runtime * 0.72
                 today = Time.today_day(self)
                 month = Time.today_month(self)
-                db.iotConInsert(IoTSpace.num, fan_consumption, today, month)
+                db.iotFanInsert(set_information.fan1, fan_runtime, fan_consumption, today, month)
+                self.fan_status.update_status(0)
 
-            add_number()
             self.fan_status.update_status(0)
             print("온도가 낮으므로 쿨링팬이 작동하지 않습니다.")
 
-            db.iotDhtInsert(IoTSpace.num, set_information.dht1, timestamp, temperature, humidity)
+            db.iotDhtInsert(set_information.dht1, timestamp, temperature, humidity)
             GPIO.output(set_gpio.iot_dht_fan, GPIO.HIGH)
