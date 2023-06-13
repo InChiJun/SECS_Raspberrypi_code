e512a8fcf3ca9782011f2631ffe755f011310eb9
Donghwi Kim <ehdgnl0904@naver.com>
2023-06-13 18:23:45 +0900

    request 모듈로 센서 데이터를 서버로 전송

--- AIoT_Space/sensor.py
+++ AIoT_Space/sensor.py
@@ -1,3 +1,5 @@
+import json
+import requests
 import RPi.GPIO as GPIO
 import Adafruit_DHT as dht
 
@@ -31,15 +33,30 @@ class AIoTSpace:
         humidity, temperature = dht.read_retry(dht.DHT22, set_gpio.aiot_dht)        # 온습도 정보를 읽어온다
         aiot_ir_state = GPIO.input(set_gpio.aiot_ir)        # IR 센서의 감지 정보를 읽어온다
 
+        def get_aiot_ir_state():
+            if aiot_ir_state == 0:
+                return '물체가 감지됨'
+            else:
+                return '감지되지 않음'
+
+        payload = {
+            'temperature': temperature,
+            'ior_ir_state': get_aiot_ir_state()
+        }
+
+        url = 'http://127.0.0.1:8000/aiotspace/'
+        headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+        response = requests.post(url, data=json.dumps(payload), headers=headers)
+
         if aiot_ir_state == 0:      # IR 센서에 물체가 감지되었을 때
             if self.bulb_status.get_current_status() != 1:      # 전구가 꺼져있는 상태일 때(전구 상태가 0이었을 때)
                 tc.set_aiot_bulb_start_time()  # 전구가 켜진 시간을 받아온다
 
             self.bulb_status.update_status(1)       # 전구의 현재 상태를 1(켜짐)로 바꾼다
-            print(timestamp, "-- Signal detected!")
+            print(timestamp, "-- 물체가 감지됨(AIoT Space)")
 
-            db.aiotIrInsert(set_information.ir2, timestamp, set_information.detect)
             GPIO.output(set_gpio.aiot_ir_bulb, GPIO.LOW)        # 전구를 작동시킨다
+            return response
 
         elif aiot_ir_state == 1:        # IR 센서에 물체가 감지되지 않았을 때
             if self.bulb_status.get_current_status() != 0:      # 전구가 켜져있다가 꺼졌을 때(전구 상태가 1이었을 때)
@@ -53,12 +70,12 @@ class AIoTSpace:
                 self.bulb_status.update_status(0)       # 전구의 상태를 다시 0(꺼짐)으로 바꾼다
 
             self.bulb_status.update_status(0)       # 전구의 현재 상태를 0(꺼짐)으로 설정한다
-            print(timestamp, "-- Signal not detected!")
+            print(timestamp, "--감지되지 않음(AIoT Space)")
 
-            db.aiotIrInsert(set_information.ir2, timestamp, set_information.detect)
             GPIO.output(set_gpio.aiot_ir_bulb, GPIO.HIGH)
+            return response
 
-        print("AIoT_Temperature={0:0.1f}*C AIoT_Humidity={1:0.1f}%".format(temperature, humidity))
+        print("Temperature={0:0.1f}*C / Humidity={1:0.1f}% (AIoT Space)".format(temperature, humidity))
 
         if temperature > 21.0:
             if self.fan_status.get_current_status() != 1:
@@ -67,8 +84,8 @@ class AIoTSpace:
             self.fan_status.update_status(1)
             print("온도가 높으므로 쿨링팬을 작동합니다.")
 
-            db.aiotDhtInsert(set_information.dht2, timestamp, temperature, humidity)
             GPIO.output(set_gpio.aiot_dht_fan, GPIO.LOW)
+            return response
 
         elif temperature <= 21.0:
             if self.fan_status.get_current_status() != 0:
@@ -84,5 +101,5 @@ class AIoTSpace:
             self.fan_status.update_status(0)
             print("온도가 낮으므로 쿨링팬이 작동하지 않습니다.")
 
-            db.aiotDhtInsert(set_information.dht2, timestamp, temperature, humidity)
             GPIO.output(set_gpio.aiot_dht_fan, GPIO.HIGH)
+            return response
--- IoT_Space/sensor.py
+++ IoT_Space/sensor.py
@@ -1,3 +1,5 @@
+import json
+import requests
 import RPi.GPIO as GPIO
 import Adafruit_DHT as dht
 
@@ -31,15 +33,29 @@ class IoTSpace:
         humidity, temperature = dht.read_retry(dht.DHT22, set_gpio.iot_dht)     # 온습도 정보를 읽어온다
         iot_ir_state = GPIO.input(set_gpio.iot_ir)      # IR 센서의 감지 정보를 읽어온다
 
+        def get_iot_ir_state():
+            if iot_ir_state == 0:
+                return '물체가 감지됨'
+            else:
+                return '감지되지 않음'
+
+        payload = {
+            'temperature': temperature,
+            'ior_ir_state': get_iot_ir_state()
+        }
+
+        url = 'http://127.0.0.1:8000/iotspace/'
+        headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+        response = requests.post(url, data=json.dumps(payload), headers=headers)
+
         if iot_ir_state == 0:       # IR 센서에 물체가 감지되었을 때
             if self.bulb_status.get_current_status() != 1:      # 전구가 꺼져있는 상태일 때(전구 상태가 0이었을 때)
                 tc.set_iot_bulb_start_time()        # 전구가 켜진 시간을 받아온다
 
             self.bulb_status.update_status(1)       # 전구의 현재 상태를 1(켜짐)로 바꾼다
-            print(timestamp, "-- Signal detected!")
-
-            db.iotIrInsert(set_information.ir1, timestamp, set_information.detect)
+            print(timestamp, "-- 물체가 감지됨(IoT Space)")
             GPIO.output(set_gpio.iot_ir_bulb, GPIO.LOW)      # 전구를 작동시킨다
+            return response
 
         elif iot_ir_state == 1:     # IR 센서에 물체가 감지되지 않았을 때
             if self.bulb_status.get_current_status() != 0:      # 전구가 켜져있다가 꺼졌을 때(전구 상태가 1이었을 때)
@@ -53,12 +69,12 @@ class IoTSpace:
                 self.bulb_status.update_status(0)       # 전구의 상태를 다시 0(꺼짐)으로 바꾼다
 
             self.bulb_status.update_status(0)  # 전구의 현재 상태를 0(꺼짐)으로 설정한다
-            print(timestamp, "-- Signal not detected!")
+            print(timestamp, "-- 감지되지 않음(IoT Space)")
 
-            db.iotIrInsert(set_information.ir1, timestamp, set_information.non_detect)
             GPIO.output(set_gpio.iot_ir_bulb, GPIO.HIGH)
+            return response
 
-        print("IoT_Temperature={0:0.1f}*C IoT_Humidity={1:0.1f}%".format(temperature, humidity))
+        print("Temperature={0:0.1f}*C / Humidity={1:0.1f}% (IoT Space)".format(temperature, humidity))
 
         if temperature > 21.0:
             if self.fan_status.get_current_status() != 1:
@@ -67,8 +83,8 @@ class IoTSpace:
             self.fan_status.update_status(1)
             print("온도가 높으므로 쿨링팬을 작동합니다.")
 
-            db.iotDhtInsert(set_information.dht1, timestamp, temperature, humidity)
             GPIO.output(set_gpio.iot_dht_fan, GPIO.LOW)
+            return response
 
         elif temperature <= 21.0:
             if self.fan_status.get_current_status() != 0:
@@ -84,5 +100,5 @@ class IoTSpace:
             self.fan_status.update_status(0)
             print("온도가 낮으므로 쿨링팬이 작동하지 않습니다.")
 
-            db.iotDhtInsert(set_information.dht1, timestamp, temperature, humidity)
             GPIO.output(set_gpio.iot_dht_fan, GPIO.HIGH)
+            return response
